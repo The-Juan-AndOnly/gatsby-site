ec13a10505b233a7a2cbb0bc86300d1d7f5af8c8
Juan Lopez <juanl0215@gmail.com>
2019-02-23 10:54:35 -0500

    testing variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,9 @@
 /* eslint-disable no-undef */
 /* eslint-disable max-len */
+
+require('dotenv').config({
+  path: `.env.${ process.env.NODE_ENV }`
+})
 module.exports = {
   siteMetadata: {
     title: `Vince's Cheesesteaks`,
@@ -26,6 +30,14 @@ module.exports = {
       resolve: `gatsby-plugin-styled-components`,
       options: {
         // Add any options here
+      }
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.SPACE_ID,
+        // Learn about environment variables: https://gatsby.app/env-vars
+        accessToken: process.env.ACCESS_TOKEN
       }
     },
     `gatsby-transformer-sharp`,
