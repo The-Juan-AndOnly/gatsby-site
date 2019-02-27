5a213b70fbe23e9d87f38bc6c77f5fb0c0c6078d
Juan Lopez <juanl0215@gmail.com>
2019-02-27 17:36:45 -0500

    Added Netlify Form

--- src/components/ContactPageComponent/ContactForm.js
+++ src/components/ContactPageComponent/ContactForm.js
@@ -14,7 +14,7 @@ const ContactForm = () => {
           data-netlify="true"
           data-netlify-honeypot="bot-field"
         >
-          <input type="hidden" name="bot-field" value="contact" />
+          <input type="hidden" name="form-name" value="contact" />
           <div className="form-group">
             <label htmlFor="name">Name:</label>
             <input type="text" name="name" id="name" />
