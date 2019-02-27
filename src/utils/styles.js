c121146ebd0b86b99ea403ee58031d590ca0cda7
Juan Lopez <juanl0215@gmail.com>
2019-02-27 18:34:30 -0500

    Updated styles.js

--- src/utils/styles.js
+++ src/utils/styles.js
@@ -3,7 +3,7 @@ export const colors = {
   mainBlack: `#262626`,
   mainYellow: `#d2aa5c`,
   mainYellow2: `#F2AF29`,
-  mainGrey: `#474747`,
+  mainGrey: `#474747`
 }
 export const textSlanted = `font-family:'Caveat', cursive;`
 
@@ -13,32 +13,32 @@ export const transFunction = (
   time = '0.5s',
   type = 'linear'
 ) => {
-  return `transition:${property} ${time} ${type}`
+  return `transition:${ property } ${ time } ${ type }`
 }
 export const transObject = ({
   property = 'all',
   time = '0.5s',
-  type = 'ease-in-out',
+  type = 'ease-in-out'
 }) => {
-  return `transition: ${property} ${time} ${type}`
+  return `transition: ${ property } ${ time } ${ type }`
 }
 
 export const transition = ({
   property = 'all',
   time = '0.5s',
-  type = 'ease-in-out',
+  type = 'ease-in-out'
 }) => {
-  return `transition: ${property} ${time} ${type}`
+  return `transition: ${ property } ${ time } ${ type }`
 }
 
 export const border = ({
   width = '0.15rem',
   type = 'solid',
-  color = 'white',
+  color = 'white'
 }) => {
-  return `border:${width} ${type} ${color}`
+  return `border:${ width } ${ type } ${ color }`
 }
 
 export const letterSpacing = ({ spacing = '0.1rem' }) => {
-  return `letter-spacing:${spacing}`
+  return `letter-spacing:${ spacing }`
 }
