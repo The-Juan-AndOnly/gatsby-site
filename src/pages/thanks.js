ce413ae77bb935d5287b1c24fabfe380e03a5c75
Juan Lopez <juanl0215@gmail.com>
2019-02-27 16:42:35 -0500

    Added Thank You page

---
+++ src/pages/thanks.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+import SEO from '../components/seo'
+const ThanksPage = () => (
+  <Layout>
+    <SEO
+      title="Contact"
+      keywords={[`Vince's`, `cheesesteak`, `Allentown,PA`]}
+    />
+    <h1> Thank You</h1>
+  </Layout>
+)
+
+export default ThanksPage
